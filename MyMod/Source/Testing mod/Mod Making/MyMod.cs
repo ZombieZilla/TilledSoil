95939e73b651b2f283a59b19d711eeb818408473
ZombieZilla <48612811+ZombieZilla@users.noreply.github.com>
2020-03-27 21:34:44 +1100

    Add files via upload

---
+++ MyMod/Source/Testing mod/Mod Making/MyMod.cs
@@ -0,0 +1,29 @@
+﻿using Verse;
+
+namespace Tilled_Soil_Zilla
+{
+    [StaticConstructorOnStartup]
+    public class LaunchLog
+    {
+        static LaunchLog()
+        {
+            Log.Message("Fingers crossed!");
+        }
+    }
+
+    public class TilledSoilSettings : ModSettings
+    {
+        public float fertility = 100;
+        public float Fertility => fertility / 100f;
+        public bool needsoil = true;
+
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref this.fertility, "fertility", 100, false);
+            Scribe_Values.Look(ref needsoil, "NeedSoil", true);
+        }
+ 
+    }
+}
