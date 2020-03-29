15a1d478d558dfd6174433c05169b9b5cec954a1
ZombieZilla <joshuasander04@gmail.com>
2020-03-29 16:25:08 +1100

    Add

---
+++ TilledSoil/Source/Testing mod/Mod Making/ModSettings.cs
@@ -0,0 +1,52 @@
+﻿using Verse;
+
+namespace Tilled_Soil_Zilla
+{
+
+    [StaticConstructorOnStartup]
+    public class LaunchLog
+    {
+        static LaunchLog()
+        {
+            Log.Message("Fingers crossed!");
+            TilledSoilMod.ApplySettings();
+            if (TilledSoilMod.settings.requirecost == true)
+            {
+                TilledSoilMod.ApplySettings2();
+            }
+            else
+            {
+                return;
+            }
+
+            
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public class TilledSoilSettings : ModSettings
+    {
+        public float fertility = 120;
+        public float Fertility => fertility / 100f;
+        public bool placeonanything = false;
+        public bool requirecost = false;
+        public int soilcost = 1;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref this.fertility, "fertility", 120);
+            Scribe_Values.Look(ref placeonanything, "PlaceOnAnything", false);
+            Scribe_Values.Look(ref requirecost, "RequireCost", false, true);
+            Scribe_Values.Look(ref soilcost, "SoilCost", 1);
+            base.ExposeData();
+        }
+
+        public void ResetSettings()
+        { 
+             fertility = 120;
+             placeonanything = false;
+             requirecost = false;
+             soilcost = 1;
+        }
+    }
+}
---
+++ TilledSoil/Source/Testing mod/Mod Making/TilledSoilMod.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using System.Reflection;
+using Verse.Sound;
+using UnityEngine;
+
+namespace Tilled_Soil_Zilla
+{
+
+    public class TilledSoilMod : Mod
+    {
+
+        public static TilledSoilSettings settings;
+
+        public TilledSoilMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<TilledSoilSettings>();
+        }
+
+        public static void ApplySettings()
+        {
+
+            BuildableDef fertility = TerrainDef.Named("Ti" +
+                "lledSoil");
+            fertility.description = (Translator.Translate("somethingsomethingsomething"));
+            fertility.fertility = settings.Fertility;
+
+        }
+        public static void ApplySettings2()
+        {
+            BuildableDef cost = TerrainDef.Named("TilledSoil");
+            cost.costList = new List<ThingDefCountClass>() { new ThingDefCountClass(ThingDefOf.WoodLog, settings.soilcost) };
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Tilled Soil";
+        }
+        
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+
+            string str1 = settings.fertility.ToString();
+            string str2 = settings.soilcost.ToString();
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.ColumnWidth = 200f;
+            listingStandard.Label("Fertility %", -1f);
+            listingStandard.Gap(10f);
+            listingStandard.Label("Place on anything", -1f);
+            listingStandard.Gap(10f);
+            listingStandard.Label("Requires Wood", -1f);
+            listingStandard.Gap(10f);
+            listingStandard.Label("Construction cost in wood", -1f);
+            listingStandard.Gap(10f);
+            if (listingStandard.ButtonText("Reset", null))
+            {
+                settings.fertility = 120f;
+                str1 = "100";
+                settings.placeonanything = false;
+                settings.requirecost = false;
+                settings.soilcost = 1;
+                str2 = "1";
+            }
+            listingStandard.NewColumn();
+            listingStandard.ColumnWidth = 200f;
+
+            float previousfertility = settings.fertility;
+            listingStandard.TextFieldNumeric(ref settings.fertility, ref str1, 70, 10000);
+            if (settings.fertility != previousfertility)
+            {
+                ApplySettings();
+            }
+            listingStandard.Gap(10f);
+
+            bool previousTerrainAffordanceDef = settings.placeonanything;
+            listingStandard.CheckboxLabeled("", ref settings.placeonanything);
+            TerrainAffordanceDef placeonanything = settings.placeonanything ? TerrainAffordanceDefOf.Light : TerrainAffordanceDefOf.GrowSoil;
+            if (settings.placeonanything != previousTerrainAffordanceDef)
+            {
+                TerrainDef soilplacement = TerrainDef.Named("TilledSoil");
+                soilplacement.description = Translator.Translate("TilledSoilDesc");
+                soilplacement.terrainAffordanceNeeded = placeonanything;
+            }
+            listingStandard.Gap(10f);
+
+ 
+            bool previousState = settings.requirecost;
+            listingStandard.CheckboxLabeled("", ref settings.requirecost);
+            if (settings.requirecost != previousState)
+            {
+                BuildableDef togglecost = TerrainDef.Named("TilledSoil");
+                togglecost.costList = new List<ThingDefCountClass>() { new ThingDefCountClass(null, 0) };
+                //ApplySettings();
+            }
+            listingStandard.Gap(10f);
+
+            int previousCost = settings.soilcost;
+            listingStandard.TextFieldNumeric(ref settings.soilcost, ref str2, 0, 100);
+           // if (settings.requirecost == true)
+           // {
+            //    ApplySettings2();
+            //}
+            listingStandard.NewColumn();
+            listingStandard.ColumnWidth = 500f;
+            listingStandard.Label("100% = Normal soil, 140% = Rich soil, 120% is default.");
+            listingStandard.Gap(10f);
+            listingStandard.Label("Toggles between placing on only soil and everything.");
+            listingStandard.Gap(10f);
+            listingStandard.Label("Does the Tilled Soil require wood to construct?");
+            listingStandard.Gap(10f);
+            listingStandard.Label("How much wood is required for 1 tile.");
+            listingStandard.Gap(10f);
+            listingStandard.Label("RESTART TO APPLY CHANGES");
+            listingStandard.End();
+            settings.Write();
+        }
+    }
+}
